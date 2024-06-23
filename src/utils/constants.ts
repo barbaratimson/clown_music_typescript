6ac1af4f1835bb60d23b6a84b3198c0144a902e8
barbaratimson <cokolek.sokol2014@mail.ru>
2024-06-23 10:10:15 +0000

    fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
-// @ts-ignore
-export const [userId,userToken] = localStorage.getItem("Authorization")?.split(":")
+export const userId = localStorage.getItem("Authorization")?.split(":")[0] ?? ""
+export const userToken = localStorage.getItem("Authorization")?.split(":")[1] ?? ""
 export const link = process.env.REACT_APP_YMAPI_LINK
