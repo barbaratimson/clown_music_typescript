8c818d9097a922eaf2f346840cb428ee1a6db1ad
Sokolow Sergey <51046717+barbaratimson@users.noreply.github.com>
2024-06-23 16:26:20 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-        <BrowserRouter>
+        <BrowserRouter basename="/index.html">
     <Provider store={store}>
      {/*<React.StrictMode>*/}
         <Main/>
