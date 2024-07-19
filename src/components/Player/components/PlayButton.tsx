36c2c4fc85d0dbf222cb5c596870a1a6bdadea17
barbaratimson <cokolek.sokol2014@mail.ru>
2024-07-19 16:59:53 +0000

    forgot

---
+++ src/components/Player/components/PlayButton.tsx
@@ -0,0 +1,36 @@
+import {IconButton} from "@mui/material";
+import { PauseRounded, PlayArrowRounded } from "@mui/icons-material";
+
+interface PlayButtonProps {
+    playing: boolean,
+    startFunc: Function,
+    stopFunc: Function,
+    onKeyDown: Function
+    className?: string
+}
+
+const PlayButton = ({playing,startFunc,stopFunc,onKeyDown,className}:PlayButtonProps)=>{
+    return (
+        <>
+            <IconButton
+                className={`player-primary-button play ${className}`}
+                key={`player-button-play-${playing}`}
+                aria-label={playing ? 'play' : 'pause'}
+                onClick={() => {
+                    !playing ? startFunc() : stopFunc()
+                }}
+                onKeyDown={(e) => {
+                   onKeyDown(e)
+                }}
+            >
+                {!playing ? (
+                    <PlayArrowRounded />
+                ) : (
+                    <PauseRounded />
+                )}
+            </IconButton>            
+        </>
+    )
+}
+
+export default PlayButton
