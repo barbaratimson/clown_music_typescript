ac60b5c374f8d99aa28f53911181c6d5b26a7751
barbaratimson <cokolek.sokol2014@mail.ru>
2024-08-17 14:49:42 +0000

    added playlist arrangement component

---
+++ src/components/PlaylistsBlock/index.tsx
@@ -0,0 +1,34 @@
+import { AlbumT, EmptyAlbumT, PlaylistT } from "../../utils/types/types"
+import PlaylistCard from "../PlaylistCard"
+
+interface PlaylistsBlockT {
+    type: "grid" | "flex"
+    playlists: Array<PlaylistT>
+}
+
+interface AlbumsBlockT {
+    type: "grid" | "flex"
+    albums: Array<EmptyAlbumT>
+}
+
+export const PlaylistsBlock = ({playlists, type}:PlaylistsBlockT) => {
+    return (
+        <div key={type} className={`animated-opacity-4ms ${type === "grid" ? "playlists-wrapper-grid" : "playlists-wrapper-flex"}`}>
+        {playlists ? playlists.map((playlist) => playlist.kind !== 0 ? (
+            <PlaylistCard type={type === "grid" ? "block" : "line"} key={playlist.kind} title={playlist.title} coverUri={playlist.cover.uri} link={`/users/${playlist.owner.uid}/playlist/${playlist.kind}`} />
+        ) : null
+    ) : null}
+        </div>
+    )
+}
+
+export const AlbumsBlock = ({albums, type}:AlbumsBlockT) => {
+    return (
+        <div key={type} className={`animated-opacity-4ms ${type === "grid" ? "playlists-wrapper-grid" : "playlists-wrapper-flex"}`}>
+        {albums ? albums.map(album => (
+            <PlaylistCard type={type === "grid" ? "block" : "line"} key={album.id} title={album.title} coverUri={album.coverUri} link={`/artist/${album.artists[0].id}/album/${album.id}`} />
+        )) : null}
+        </div>
+    )
+}
+
